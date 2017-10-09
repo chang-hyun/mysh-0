f527f2e72c66afc986c01a9c5e95f0e11dc30b5d
Changhyun Kim <chang9403@ajou.ac.kr>
2017-10-09 14:45:08 +0900

    Added parsing, cd, pwd

--- src/commands.c
+++ src/commands.c
@@ -1,11 +1,12 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+  
   // TODO: Fill it!
   return 0;
 }
@@ -15,16 +16,27 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-
+  char dir[1000];
+  getcwd(dir, 1000);
+  printf("%s\n", dir);
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  
+  if(strcmp(argv[0], "cd")!=0 || argc != 2)
+  {
+    return 0;
+  }
+  else if(chdir(argv[argc-1])<0) return 0;
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  char dir[1000];
+  if(strcmp(argv[0], "pwd")!=0 || argc >= 2)
+  {
+    return 0;
+  }
   return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,45 @@
 #include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+  *argc = 0;
+  
+  char cmd[100];
+  char restraints[] = " \t\n";
+  strcpy(cmd, command);
+  
+  char* result;
+  result = strtok(cmd, restraints);
+
+  if(result != NULL)
+  {
+    
+    *argv = (char**)malloc(100 * sizeof(char*));
+    (*argv)[*argc] = (char*)malloc((strlen(result)) * sizeof(char));
+    strcpy((*argv)[*argc], result);
+    (*argc)++;
+  }
+
+  else
+  {
+    *argv = (char**)malloc(sizeof(char*));
+    *argv[0] = (char*)malloc(sizeof(char));
+    strcpy((*argv)[0], "");
+    (*argc) = 1;
+  }  
+
+  while (result != NULL)
+  {
+    result = strtok(NULL, restraints);
+    if(result != NULL)
+    {
+    (*argv)[*argc] = (char*)malloc((strlen(result))*sizeof(char));
+    strcpy((*argv)[*argc], result);
+    (*argc)++;
+    }
+  }
 }
